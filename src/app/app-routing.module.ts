26d561114f547813e249907ec36f6e50837e7240
Mostafa <mostafahr@hotmail.com>
2020-11-03 21:16:02 -0800

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DocumentationComponent} from './documentation/documentation.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'documentation',
+    component: DocumentationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentationComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
